341caefac941908f39c2e559b5bfaa0cf1aee718
gabrielkf <gabrielkf@gmail.com>
2022-04-07 00:29:45 -0300

    Add query parameter to /users

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,10 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import {
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
@@ -10,9 +15,10 @@ export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @ApiOkResponse({ type: User, isArray: true, description: 'In memory users' })
+  @ApiQuery({ name: 'name', required: false })
   @Get()
-  getUsers(): User[] {
-    return this.usersService.findAll();
+  getUsers(@Query('name') name: string): User[] {
+    return this.usersService.findAll(name);
   }
 
   @ApiOkResponse({ type: User })
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,12 +2,44 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/user.entity';
 
+const SAMPLE_USERS = [
+  {
+    id: 1649300345000,
+    name: 'Mario Bros',
+  },
+  {
+    id: 1649300345330,
+    name: 'Bruna Venus',
+  },
+  {
+    id: 1649300712410,
+    name: 'John Travolta',
+  },
+  {
+    id: 1649300720779,
+    name: 'Abigail Travolta',
+  },
+  {
+    id: 1649300739599,
+    name: 'Arnold Bros',
+  },
+  {
+    id: 1649300739999,
+    name: 'Arnold Schwarzenegger',
+  },
+];
+
 @Injectable()
 export class UsersService {
-  private users: User[] = [{ id: 0, name: 'Mario' }];
+  private users: User[] = [...SAMPLE_USERS];
 
-  findAll(): User[] {
-    return this.users;
+  findAll(name?: string): User[] {
+    return !name
+      ? this.users
+      : this.users.filter((user) => {
+          const nameRegex = new RegExp(name, 'i');
+          return nameRegex.test(user.name);
+        });
   }
 
   findById(userId: number): User {
